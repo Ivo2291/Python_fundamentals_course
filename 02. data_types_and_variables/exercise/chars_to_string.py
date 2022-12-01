97c6840ff6b86e7ca966f3dbe6b745002eca4147
Ivo Todorov <ivo2291@gmail.com>
2022-12-01 11:35:38 +0200

    Add files via upload

---
+++ 02. data_types_and_variables/exercise/chars_to_string.py
@@ -0,0 +1,11 @@
+a, b, c = input(), input(), input()
+
+print(f"{a}{b}{c}")
+
+# second_solution
+
+first_char = input()
+second_char = input()
+third_char = input()
+
+print(f"{first_char}{second_char}{third_char}")
