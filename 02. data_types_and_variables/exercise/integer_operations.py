30c18834b905ac449bc701d8771943573fb85fb2
Ivo Todorov <ivo2291@gmail.com>
2022-12-01 11:37:05 +0200

    Add files via upload

---
+++ 02. data_types_and_variables/exercise/integer_operations.py
@@ -0,0 +1,16 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+
+result = int((a + b) / c) * d
+
+print(result)
+
+# second_solution
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+forth_number = int(input())
+
+result = int((first_number + second_number) / third_number) * forth_number
+
+print(result)
