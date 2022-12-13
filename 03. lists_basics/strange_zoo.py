225370e042387fb9f45937fe9ed5f21f6ae90128
Ivo Todorov <ivo2291@gmail.com>
2022-12-13 16:52:27 +0200

    Add files via upload

---
+++ 03. lists_basics/strange_zoo.py
@@ -0,0 +1,19 @@
+tail = input()
+body = input()
+head = input()
+
+meerkat = [tail, body, head]
+
+meerkat[0], meerkat[2] = meerkat[2], meerkat[0]
+
+print(meerkat)
+
+# second_solution
+
+tail = input()
+body = input()
+head = input()
+
+meerkat = [head, body, tail]
+
+print(meerkat)
