03b1a2c868363d37764accd335e058cbaabd59e4
Ivo Todorov <ivo2291@gmail.com>
2022-12-21 14:29:33 +0200

    Add files via upload

---
+++ 04. functions/exercise/min_max_and_sum.py
@@ -0,0 +1,5 @@
+sequence_of_numbers = list(map(int, input().split()))
+
+print(f"The minimum number is {min(sequence_of_numbers)}")
+print(f"The maximum number is {max(sequence_of_numbers)}")
+print(f"The sum number is: {sum(sequence_of_numbers)}")
